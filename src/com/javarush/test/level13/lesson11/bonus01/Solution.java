0d33fdfd5922a747abbf7735c3c1eea92ec2fe20
Egor <egaby@mail.ru>
2015-04-29 18:58:57 +0300

    from home

--- src/com/javarush/test/level13/lesson11/bonus01/Solution.java
+++ src/com/javarush/test/level13/lesson11/bonus01/Solution.java
@@ -17,13 +17,32 @@ package com.javarush.test.level13.lesson11.bonus01;
 10
 */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Solution
-{
+public class Solution {
     public static void main(String[] args) throws IOException {
         // напишите тут ваш код
-       
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                reader = new BufferedReader(new FileReader(reader.readLine()));
+        List<Integer> list = new ArrayList<>();
+        while (reader.ready()) {
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        Collections.sort(list);
+        int target = 0;
+        for (int i = 0; i < list.size(); i++) {
+            target = list.get(i);
+            if (target % 2 == 0) {
+                System.out.println(target);
+            }
 
+
+        }
     }
 }
