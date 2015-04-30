fe8225b3be34731928835ec970d2d4e04ec481cc
Egor <egaby@mail.ru>
2015-05-01 00:56:39 +0300

    chapter 14 is almost done

---
+++ src/com/javarush/test/level14/lesson08/home09/USD.java
@@ -0,0 +1,1 @@
+package com.javarush.test.level14.lesson08.home09;/** * Created by Egor on 30.04.2015. */public class USD extends Money {    public USD(double amount) {        super(amount);    }    @Override    public String getCurrencyName() {        return "USD";    }}
