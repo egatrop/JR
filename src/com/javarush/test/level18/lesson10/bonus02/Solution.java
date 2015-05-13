c09fe139a995bd42b2bc369ef3aa8964ba17bf42
Egor <egaby@mail.ru>
2015-05-14 00:27:24 +0300

    chapter 14 is almost done`

--- src/com/javarush/test/level18/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus02/Solution.java
@@ -19,7 +19,7 @@ id productName price quantity
 Пример:
 19846   Шорты пляжные синие           159.00  12
 198478  Шорты пляжные черные с рисунко173.00  17
-19847983Куртка для сноубордистов, разм10173.991234    e:\price.txt
+19847983Куртка для сноубордистов, разм10173.991234
 */
 
 import java.io.*;
@@ -30,26 +30,46 @@ public class Solution {
         String fileName = reader.readLine();
         reader.close();
 
-        File file = new File(fileName);
+        if ("-c".equals(args[0])) {
 
-        String id = append(8, Integer.toString((int) ((file.length()) / 81 + 1)));
-        System.out.println(file.length());
-        String productName = append(60, args[1]);
-        String price = append(8, args[2]);
-        String quantity = append(4, args[3]);
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+            String tempStr = reader.readLine();
+            String countStr = "0       ";
+            while(true) {
+                if (tempStr == null){
+                    break;
+                }
+                countStr = tempStr;
+                tempStr = reader.readLine();
+            }
+            reader.close();
+            String s1 = countStr.substring(0, 8);
+            String s2 = s1.trim();
+            int count = Integer.parseInt(s2);
 
-        OutputStream out = new FileOutputStream(fileName, true);
-        out.write(id.getBytes());
-        out.write(productName.getBytes());
-        out.write(price.getBytes());
-        out.write(quantity.getBytes());
-        out.write("\n".getBytes());
+            StringBuilder builder = new StringBuilder("");
+            for (int i = 1; i <= args.length - 3; i++) {
+                builder.append(args[i]).append(" ");
+            }
+            String id = append(8, Integer.toString(count + 1));
+            String productName = append(30, builder.toString());
+            String price = append(8, args[args.length - 2]);
+            String quantity = append(4, args[args.length - 1]);
 
+            OutputStream out = new FileOutputStream(fileName, true);
+            out.write(id.getBytes());
+            out.write(productName.getBytes());
+            out.write(price.getBytes());
+            out.write(quantity.getBytes());
+            out.write("\n".getBytes());
+            out.flush();
+            out.close();
+        }
     }
 
     private static String append(int count, String s) {
-        if ( s.length() > count) {
-            s = s.substring(0, count + 1);
+        if (s.length() > count) {
+            s = s.substring(0, count);
         } else {
             int append = count - s.length();
             for (int i = 0; i < append; i++) {
